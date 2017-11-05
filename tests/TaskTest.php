2f9931036ad9d83dcdc1e4d494a3b51978595399
JasonSekhon <jason_2s@hotmail.com>
2017-11-05 02:27:00 -0800

    fixed tasktests

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -25,37 +25,37 @@ class TaskTest extends PHPUnit_Framework_TestCase {
 
     public function testTaskMaxLength64(){
       $value = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertEquals($value, $this->task->task);
     }
 
     public function testTaskEmpty(){
       $value = "";
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertNotEquals($value, $this->task->task);
     }
 
     public function testTaskString(){
       $value = "Task";
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertEquals($value, $this->task->task);
     }
 
     public function testTaskStringSpace(){
       $value = "Task Creation";
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertEquals($value, $this->task->task);
     }
 
     public function testTaskStringInt(){
       $value = "2";
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertEquals($value, $this->task->task);
     }
 
     public function testTaskInt(){
       $value = 2;
-      $this->task->task = $value;
+      $this->task->setTask($value);
       $this->assertNotEquals($value, $this->task->task);
     }
 
@@ -63,37 +63,37 @@ class TaskTest extends PHPUnit_Framework_TestCase {
 
     public function testPriorityString(){
       $value = "priority";
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertNotEquals($value, $this->task->priority);
     }
 
     public function testPriorityIntInRangeMin(){
       $value = 1;
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertEquals($value, $this->task->priority);
     }
 
     public function testPriorityIntInRangeMax(){
       $value = 4;
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertEquals($value, $this->task->priority);
     }
 
     public function testPriorityIntOutOfRangeAbove(){
       $value = 5;
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertNotEquals($value, $this->task->priority);
     }
 
     public function testPriorityIntOutOfRangeBelow(){
       $value = 0;
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertNotEquals($value, $this->task->priority);
     }
 
     public function testPriorityDouble(){
       $value = 2.0;
-      $this->task->priority = $value;
+      $this->task->setPriority($value);
       $this->assertNotEquals($value, $this->task->priority);
     }
 
@@ -101,37 +101,37 @@ class TaskTest extends PHPUnit_Framework_TestCase {
 
     public function testSizeString(){
       $value = "Size";
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertNotEquals($value, $this->task->size);
     }
 
     public function testSizeIntInRangeMin(){
       $value = 1;
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertEquals($value, $this->task->size);
     }
 
     public function testSizeIntInRangeMax(){
       $value = 4;
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertEquals($value, $this->task->size);
     }
 
     public function testSizeIntOutOfRangeAbove(){
       $value = 5;
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertNotEquals($value, $this->task->size);
     }
 
     public function testSizeIntOutOfRangeBelow(){
       $value = 0;
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertNotEquals($value, $this->task->size);
     }
 
     public function testSizeDouble(){
       $value = 2.0;
-      $this->task->size = $value;
+      $this->task->setSize($value);
       $this->assertNotEquals($value, $this->task->size);
     }
 
@@ -139,37 +139,37 @@ class TaskTest extends PHPUnit_Framework_TestCase {
 
     public function testGroupString(){
       $value = "Group";
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertNotEquals($value, $this->task->group);
     }
 
     public function testGroupIntInRangeMin(){
       $value = 1;
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertEquals($value, $this->task->group);
     }
 
     public function testGroupIntInRangeMax(){
       $value = 5;
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertEquals($value, $this->task->group);
     }
 
     public function testGroupIntOutOfRangeAbove(){
       $value = 6;
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertNotEquals($value, $this->task->group);
     }
 
     public function testGroupIntOutOfRangeBelow(){
       $value = 0;
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertNotEquals($value, $this->task->group);
     }
 
     public function testGroupDouble(){
       $value = 2.0;
-      $this->task->group = $value;
+      $this->task->setGroup($value);
       $this->assertNotEquals($value, $this->task->group);
     }
 }
